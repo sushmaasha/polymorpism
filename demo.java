c9971475fec18dc7d663aadd5623be0e0d2de3bb
sushmaasha <123888171+sushmaasha@users.noreply.github.com>
2023-02-01 09:04:32 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,20 @@
+package assign;
+
+class demo {
+void add(int a,int b)
+{
+	if(a==5&& b==10)
+	{
+		System.out.println("20");
+	}
+	else
+	{
+		System.out.println(a+b);
+	}
+}
+void add(int a,int b,int c)
+{
+	int d=a+b+c;
+	System.out.println(d);
+}
+}
---
+++ maindemo.java
@@ -0,0 +1,12 @@
+package assign;
+
+public class maindemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		demo d = new demo();
+		d.add(5, 10);
+
+	}
+}
